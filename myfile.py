eb5b5a11b72e346109f36a69b34d785460824161
Anubhav Mathur <150018859+anubhavm5@users.noreply.github.com>
2025-07-31 13:26:59 +0530

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -5,9 +5,9 @@ model1=pickle.load(open("HousePrice.pkl","rb"))
 
 def mydeploy():
     st.title("House Price Prediction")
-    area=st.number_input("Enter Area")
-    bedrooms=st.number_input("Enter Bedroom You Need")
-    age=st.number_input("Enter How Old House You Looking For")
+    area=st.number_input("Enter Area",min_value=0,step=100)
+    bedrooms=st.number_input("Enter Bedroom You Need",min_value=1,max_value=7,step=1)
+    age=st.number_input("Enter How Old House You Looking For",min_value=1,max_value=50,step=1)
     pred=st.button("Predict Price")
     if pred:
         df=pd.DataFrame({"area":[area],"bedrooms":[bedrooms],"age":[age]})
